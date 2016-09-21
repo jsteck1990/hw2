67a0e2a1f849b9148867c340a5cb5015b45c3fd7
jsteck1990 <jsteck1990@gmail.com>
2016-09-21 11:19:27 -0400

    EvaluateHand method working

--- CardCountGame.cs
+++ CardCountGame.cs
@@ -17,6 +17,8 @@ namespace hw2
 			foreach (Suit s in Suit.VALUES)
 				foreach (Rank r in Rank.VALUES)
 					CCDeck.AddCard (new Card (s, r));
+			
+			CCDeck.Shuffle ();
 
 				CCHand1.AddCard (CCDeck.DealOne ());
 				CCHand1.AddCard (CCDeck.DealOne ());
@@ -27,6 +29,8 @@ namespace hw2
 				CCHand1.AddCard (CCDeck.DealOne ());
 				CCHand1.AddCard (CCDeck.DealOne ());
 
+			CCDeck.Shuffle ();
+
 				CCHand2.AddCard (CCDeck.DealOne ());
 				CCHand2.AddCard (CCDeck.DealOne ());
 				CCHand2.AddCard (CCDeck.DealOne ());
@@ -37,8 +41,8 @@ namespace hw2
 				CCHand2.AddCard (CCDeck.DealOne ());
 
 
-				Console.WriteLine ("Hand 1: {0} | Value: {1}\n", CCHand1, CCHand1.EvaluateHand());
-				Console.WriteLine ("Hand 2: {0} | Value: {1}\n", CCHand2, CCHand2.EvaluateHand());
+				Console.WriteLine ("Hand 1: {0} | Score: {1}\n", CCHand1, CCHand1.EvaluateHand());
+				Console.WriteLine ("Hand 2: {0} | Score: {1}\n", CCHand2, CCHand2.EvaluateHand());
 
 			Console.WriteLine ("Cards Remaining: {0}", CCDeck.GetCardsRemaining ());
 
--- CardCountHand.cs
+++ CardCountHand.cs
@@ -6,8 +6,7 @@ namespace hw2
 {
 	public class CardCountHand : Hand
 	{
-		private static int HandValue = 0;
-		private List<int> RankValues = new List<int>();
+		int ScoreValue;
 
 		public CardCountHand ()
 		{
@@ -28,70 +27,58 @@ namespace hw2
 
 		public override int EvaluateHand ()
 		{
+			string symbol = "";
 
 			foreach (Card c in PlayerHand) {
-				RankValues.Add (c.getRank ().RankNum);
-				
-//				switch (RankValue) {
-//				case "A":
-//					RankValue = "A";
-//					HandValue += 1;
-//					break;
-//				case "2":
-//					RankValue = "2";
-//					HandValue += 2;
-//					break;
-//				case "3":
-//					RankValue = "3";
-//					HandValue += 3;
-//					break;
-//				case "4":
-//					RankValue = "4";
-//					HandValue += 4;
-//					break;
-//				case "5":
-//					RankValue = "5";
-//					HandValue += 5;
-//					break;
-//				case "6":
-//					RankValue = "6";
-//					HandValue += 6;
-//					break;
-//				case "7":
-//					RankValue = "7";
-//					HandValue += 7;
-//					break;
-//				case "8":
-//					RankValue = "8";
-//					HandValue += 8;
-//					break;
-//				case "9":
-//					RankValue = "9";
-//					HandValue += 9;
-//					break;
-//				case "10":
-//					RankValue = "10";
-//					HandValue += 10;
-//					break;
-//				case "J":
-//					RankValue = "J";
-//					HandValue += 10;
-//					break;
-//				case "Q":
-//					RankValue = "Q";
-//					HandValue += 10;
-//					break;
-//				case "K":
-//					RankValue = "K";
-//					HandValue += 10;
-//					break;
-//				default:
-//					HandValue = 0;
-//					break;
-//				}
+				symbol = c.getRank ().GetSymbol ();
+
+				switch (symbol) {
+				case "A":
+					ScoreValue += 1;
+					break;
+				case"2":
+					ScoreValue += 2;
+					break;
+				case"3":
+					ScoreValue += 3;
+					break;
+				case"4":
+					ScoreValue += 4;
+					break;
+				case"5":
+					ScoreValue += 5;
+					break;
+				case"6":
+					ScoreValue += 6;
+					break;
+				case"7":
+					ScoreValue += 7;
+					break;
+				case"8":
+					ScoreValue += 8;
+					break;
+				case"9":
+					ScoreValue += 9;
+					break;
+				case"10":
+					ScoreValue += 10;
+					break;
+				case"J":
+					ScoreValue += 10;
+					break;
+				case"Q": 
+					ScoreValue += 10;
+					break;
+				case"K": 
+					ScoreValue += 10;
+					break;
+				default:
+					ScoreValue += 0;
+					break;
+				}
 			}
 			
-			return HandValue;
+			return ScoreValue;
 		}
 
 	}
